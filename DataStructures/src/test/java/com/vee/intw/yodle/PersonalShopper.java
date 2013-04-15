59b6e0291bbe69a0a4ff6776b89e3d9a3bfbf5ff
Narayanan V <narayanan.vn@gmail.com>
2013-04-14 23:57:43 -0400

    new interviews

---
+++ DataStructures/src/test/java/com/vee/intw/yodle/PersonalShopper.java
@@ -0,0 +1,14 @@
+package com.vee.intw.yodle;
+/**
+ * 
+ * @author Narayanan
+ * Design a Personal Shopper system
+ * 1 Client 
+ * 1 Personal Shopper
+ * N Unique Items
+ * Each Item is present in one of the M stores whose knowledge Shopper has
+ * Purchase the items and give it back to Client
+ */
+public class PersonalShopper {
+
+}
