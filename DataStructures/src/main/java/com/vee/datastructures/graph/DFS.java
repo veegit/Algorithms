bb4e17d76286647454b1498772dbe84ba4d47ab6
veegit <narayanan.vn@gmail.com>
2013-06-24 12:51:31 -0400

    new programs

--- DataStructures/src/main/java/com/vee/datastructures/graph/DFS.java
+++ DataStructures/src/main/java/com/vee/datastructures/graph/DFS.java
@@ -7,6 +7,20 @@ import java.util.TreeMap;
 import com.vee.datastructures.LinkedList;
 import com.vee.datastructures.LinkedListIterator;
 
+/*
+* Tree edges: edge (u; v) is a tree edge if v was first discovered by exploring
+edge (u; v). The tree edges form a spanning forest of G (no cycles).
+* Back edges: an edge (u; v) is a back edge if it connects vertex u to an
+ancestor v in a DFS tree.
+* Forward edges: an edge (u; v) is a forward edge if it connects a vertex u
+to a descendant v in a DFS tree.
+* Cross edges: all other edges.
+It is easy to show that:
+* (u; v) is a tree edge or a forward edge , d[u] < d[v] < f[v] < f[u]
+* (u; v) is back edge , d[v] < d[u] < f[u] < f[v]
+* (u; v) is a cross edge , d[v] < f[v] < d[u] < f[u]
+*/
+
 public class DFS {
     private int adjMatrix[][];
     private int size;
@@ -26,11 +40,12 @@ public class DFS {
 	}
     
     private void dfs(int v) {
-		visited[v] = 1;
+    	visited[v] = 1;
 		previsit[v] = clock++;
 		LinkedList<Integer> vList = adjList[v];
 		for (LinkedListIterator<Integer> iterator = vList.iterator(); iterator.hasNext();) {
 			int w = iterator.getNext();
+			check(v,w);
 			if(visited[w] == 0)
 				dfs(w);
 		}
@@ -38,11 +53,21 @@ public class DFS {
 		order.put(clock,v);
 	}
     
+    private boolean check(int u, int v) {
+    	if(previsit[v] > 0 && postvisit[v] == 0)
+			System.out.println("BackEdge " + (u+1) + "->" + (v+1));
+		else if(previsit[u] > previsit[v] && previsit[v] == 0)
+			System.out.println("FwdEdge " + (u+1) + "->" + (v+1));
+		else 
+			System.out.println("CrossEdge " + (u+1) + "->" + (v+1));
+    	return true;
+    }
+    
     private void init(int[][] adjMatrix,int size) {
     	 this.size = size;
    	     this.adjMatrix = adjMatrix;
 		 
-		 clock = 0;
+		 clock = 1;
 		 visited = new int[size];
 		 previsit = new int[size];
 		 postvisit = new int[size];
@@ -63,7 +88,7 @@ public class DFS {
 	}
     
     public static void main(String args[]) {
-		  int X[][] =
+		  int Y[][] =
 		   {
 		    {0,1,0,1,0,0,0,0,0,0},
 		    {0,0,1,0,0,1,0,0,0,0},
@@ -76,11 +101,18 @@ public class DFS {
 		    {0,0,0,0,0,0,0,1,0,0},
 		    {0,0,0,0,0,0,0,0,1,0}
 		   };
-		  int size = 10;
+		  int X[][] = 
+		  {
+				  {0,1,1,0},
+				  {0,0,0,1},
+				  {0,1,0,1},
+				  {1,0,0,0},
+		  };
+		  int size = X[0].length;
 		  for(int i=0;i<size;i++)
 			    for(int j=0;j<size;j++)
 			    	if(X[i][j] == 1)
-			    		System.out.println(i + " " + j);
+			    		System.out.println(i+1 + " " + (j+1));
 		  DFS dfs = new DFS(X,size);
 		  int previsit[] = dfs.getPrevisit();
 		  int postvisit[] = dfs.getPostvisit();
